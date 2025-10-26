35ca7f7bc4491375da5526b152a608736528d8fb
AnggraeniDwiZahra <2410817220018@mhs.ulm.ac.id>
2025-10-26 12:28:02 +0800

    Add files via upload

---
+++ modul4/src/soal1/Main1.java
@@ -0,0 +1,11 @@
+package soal1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		HewanPeliharaan pet = new HewanPeliharaan("Kucing Anggora", "Leo");
+
+		pet.display();
+	}
+
+}
